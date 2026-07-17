bb3d822ddf2b77b5ea0f002d024aabeef1559caa
ayou9889 <65youbifh@gmail.com>
2026-07-17 01:45:14 +0100

    Add files via upload

---
+++ Login/Add-Memo/assets/js/script.js
@@ -0,0 +1,40 @@
+const submit=document.querySelector("input[type='submit']");
+
+let n=null,p=null;
+if(sessionStorage.getItem("n") && sessionStorage.getItem("pw")){
+    n=sessionStorage.getItem("n");
+    p=sessionStorage.getItem("pw");
+    sessionStorage.clear();
+}
+
+let add=true;
+submit.addEventListener("click",(e)=>{
+    if(add){
+        add=false;
+        e.target.disabled=true;
+        e.target.style.backgroundColor="#6e6e6e";
+        e.preventDefault();
+        fetch("https://p01-five.vercel.app/Add-Memo",{
+            method:"POST",
+            headers:{
+                'Content-Type': 'application/json'
+            },
+            body:JSON.stringify({
+                name:n,
+                pw:p,
+                title:document.querySelector("[name='title']").value,
+                txt:document.querySelector("[name='txt']").value
+            })
+        }).then((res)=>res.json()).then((data)=>{
+                if(data.s === 1){
+                    alert("The memo was added successfully.");
+                }
+                else{
+                    alert("An error occurred.");
+                }
+                add=true;
+                e.target.disabled=false;
+                e.target.style.backgroundColor="#171717";
+            });
+    }
+});
---
+++ Login/assets/js/script.js
@@ -0,0 +1,24 @@
+const submit=document.querySelector("input[type='submit']");
+
+submit.addEventListener("click",(e)=>{
+    e.preventDefault();
+    const n=document.querySelector("[name='name']").value;
+    const pw=document.querySelector("[name='password']").value;
+    fetch("https://p01-five.vercel.app/",{
+        method:"POST",
+        headers:{
+            'Content-Type': 'application/json'
+        },
+        body:JSON.stringify({
+            name:document.querySelector("[name='name']").value,
+            password:document.querySelector("[name='password']").value
+        })
+    }).then((res)=>res.json()).then((data)=>{
+            console.log(data);
+            if(data.success){
+                sessionStorage.setItem("n",n);
+                sessionStorage.setItem("pw",pw);
+                window.location.href="https://oufaddoul.com/Memo/Add-Memo/";
+            }
+        });
+});
