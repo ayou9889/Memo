c4e6cea01e8d319044bf6a65e24a1a9e9ff8b708
ayou9889 <65youbifh@gmail.com>
2026-07-17 02:49:41 +0100

    Update script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,6 @@
 const main=document.querySelector("body>main");
 
 fetch("https://raw.githubusercontent.com/U38cI6HAghXBUZXEEn5WmPnMelniPdRPGbU1/memo/main/6589/memo.json").then((res)=>res.json()).then((d)=>{
-    console.log(d);
     d.forEach(m => {
         const div=document.createElement("div");
         div.classList.add("d");
