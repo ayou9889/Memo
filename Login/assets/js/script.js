3486009a2728ba88a352245a09cd2586e88b4971
ayou9889 <65youbifh@gmail.com>
2026-07-18 16:42:39 +0100

    Update script.js

--- Login/assets/js/script.js
+++ Login/assets/js/script.js
@@ -1,24 +1,28 @@
 const submit=document.querySelector("input[type='submit']");
-
+function a(){
+    submit.style.backcgroundColor=""#171717"";
+    return;
+}
 submit.addEventListener("click",(e)=>{
     e.preventDefault();
+    submit.style.backgroundColor="#6e6e6e";
     const n=document.querySelector("[name='name']").value;
     const pw=document.querySelector("[name='password']").value;
     if(n === ""){
         alert("You should write your name.");
-        return;
+        a();
     }
     else if(n.length < 5){
         alert("Your name should have at least 5 character.");
-        return;
+        a();
     }
     if(pw === ""){
         alert("You should write your password.");
-        return;
+        a();
     }
     else if(pw.length < 7){
         alert("Your password should have at least 7 character.");
-        return;
+        a();
     }
     fetch("https://p01-five.vercel.app/",{
         method:"POST",
@@ -30,6 +34,7 @@ submit.addEventListener("click",(e)=>{
             password:document.querySelector("[name='password']").value
         })
     }).then((res)=>res.json()).then((data)=>{
+            a();
             if(data.success){
                 sessionStorage.setItem("n",n);
                 sessionStorage.setItem("pw",pw);
