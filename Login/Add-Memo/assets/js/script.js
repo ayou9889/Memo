99de092a3eb1ebb9bd7c5eae46918064903d841d
ayou9889 <65youbifh@gmail.com>
2026-07-19 23:01:23 +0100

    Update script.js

--- Login/Add-Memo/assets/js/script.js
+++ Login/Add-Memo/assets/js/script.js
@@ -1,9 +1,11 @@
 const submit=document.querySelector("input[type='submit']");
+const sname=document.querySelector("body>h1>strong#iname");
 
 let n=null,p=null;
 if(localStorage.getItem("n") && localStorage.getItem("pw")){
     n=localStorage.getItem("n");
     p=localStorage.getItem("pw");
+    sname.innerHTML = `${n[0].toUpperCase()}${n.slice(1)}`;
     localStorage.clear();
 }
 
