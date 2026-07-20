872f2be5bd85baa229be7bcf0714a56f0a979f45
ayou9889 <65youbifh@gmail.com>
2026-07-21 00:23:59 +0100

    Update script.js

--- Login/P/assets/js/script.js
+++ Login/P/assets/js/script.js
@@ -1,7 +1,7 @@
 const sname=document.querySelector("body>h1>strong#iname");
 let n=null,p=null;
-if(sessionStorage.getItem("n") && sessionStorage.getItem("pw")){
-    n=sessionStorage.getItem("n");
-    p=sessionStorage.getItem("pw");
+if(localStorage.getItem("n") && localStorage.getItem("pw")){
+    n=localStorage.getItem("n");
+    p=localStorage.getItem("pw");
     sname.innerHTML = `${sname[0].toUpperCase()}${s.name.slice(1)}`;
 }
