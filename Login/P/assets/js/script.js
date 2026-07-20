77083483f292a2babe4dd53ebe129857467002fe
ayou9889 <65youbifh@gmail.com>
2026-07-21 00:21:45 +0100

    Update script.js

--- Login/P/assets/js/script.js
+++ Login/P/assets/js/script.js
@@ -1,4 +1,5 @@
 const sname=document.querySelector("body>h1>strong#iname");
+let n=null,p=null;
 if(sessionStorage.getItem("n") && sessionStorage.getItem("pw")){
     n=sessionStorage.getItem("n");
     p=sessionStorage.getItem("pw");
